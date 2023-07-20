4337e4803af6255c9dbe5c9dfb9f5435302c24be
Igor Mandrik <igormandrikov@mail.ru>
2023-07-20 10:48:28 +0300

    Исходная версия

---
+++ ld.py
@@ -0,0 +1,14 @@
+stock = [
+    {"name": "iPhone XR","stock": 24 ,"price": 65339, "recomented": ["iPhone X R", "Samsung S10", "Redmi 9"]},
+    {"name": "Samsung S10", "stock": 28, "price": 70339, "discount": 10},
+    {"name": "Redmi 9", "stock": 104, "price": 38995}
+]
+
+print(type(stock))
+
+print(type(stock[0]))
+
+
+
+
+
---
+++ py
@@ -0,0 +1,1 @@
+{'name': 'iPhone', 'stock': 24, 'price': 65339}
